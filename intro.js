d71fccad449e28fa5e1e0b12c13a235bd91f5f83
RemyaMohan-ai <remya04mohan@gmai.com>
2023-05-15 16:15:20 +0530

    first commit

---
+++ intro.js
@@ -0,0 +1,40 @@
+let a =23;
+let b = 34;
+
+console.log(a+b)
+
+//substraction
+let c=55;
+let d=36;
+let e=c-d;
+console.log(c ,"-", d ,"= ",e)
+
+//mutiplication
+
+let f = 34;
+let g = 25;
+h = f * g;
+console.log( f,"*",g,"=",h)
+
+// division
+
+let i=15;
+let j=5;
+k=i/j;
+console.log(i,"/",j,"=",k )
+
+
+
+// remainder
+
+let l =17;
+let m= 3;
+n=17%3;
+console.log(l,"%",m,"=",n)
+
+
+// problem
+let p = 45;
+let q =3
+R=p+b*e-q
+console.log(p,"+",b,"*",e,"-",q,"=",R)
