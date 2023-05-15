c8b9c90aec92872037cfe6c4b86fee6c74d78842
RemyaMohan-ai <remya04mohan@gmai.com>
2023-05-15 16:40:44 +0530

    result

---
+++ pass.js
@@ -0,0 +1,10 @@
+let sub1 =70;
+let sub2 = 80;
+let sub3 = 90;
+let sub4 = 50;
+let total = sub1+ sub2+sub3+sub4;
+if(total>250){
+    console.log("you passed")
+}else{
+    console.log("you failed")
+}
